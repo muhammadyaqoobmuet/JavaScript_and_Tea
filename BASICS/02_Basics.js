f09b96c723436b3c835ce72d21208468b0ac3858
muhammadyaqoobmuet <yaqoobahmed45700@gmai.com>
2024-01-09 21:45:20 +0500

    ALL BASIC OF JS

--- BASICS/02_Basics.js
+++ BASICS/02_Basics.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all jscode to newer version by means of that V8 will treat it ->newer version
 //CHAPTER02:Data types
-// A value in JavaScript is always of a certain type. For example, a string or a number.
+// a value in JavaScript is always of a certain type. For example, a string or a number.
 // There are eight basic data types in JavaScript
 
 // adding files to html file ? use <script src"your_file_location"></script>
