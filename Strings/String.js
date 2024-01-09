0f55ba8c1b6be925ae08102df6ca318dc880f398
muhammadyaqoobmuet <yaqoobahmed45700@gmai.com>
2024-01-09 21:10:02 +0500

    chanign folfers

--- Strings/String.js
+++ Strings/String.js
@@ -1,1 +1,21 @@
- 
+ // In JavaScript, the textual data is stored as strings. There is no separate type for a single character.
+ // Strings can be enclosed within either single quotes, double quotes or backticks:
+ // Strings are immutable in JS
+let single = 'single-quoted';
+let double = "double-quoted";
+let backticks = `backticks`;
+// advantage of using backticks is that they allow a string to span multiple lines:
+let guestList = `Guests:
+ * John
+ * Pete
+ * Mary
+`;
+
+// ACSESSING CHARCTER BY INDEX a
+single[0]='h'
+console.log(single[0]); // this is first index and lastindex = length-1;
+console.log(1);
+// another method to create string
+
+let email = new String("yaqoobahmed45700@gmail.com")
+console.log(email); //[String: 'yaqoobahmed45700@gmail.com']
